1184bf59eacd211e7e87c5bc1d19920e9604a1e7
eladioltb <eladioloriente@gmail.com>
2025-08-01 08:55:06 +0200

    patch: Changed region value variable to much with the main configuration

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "eu-north-1"
+  default     = "eu-west-1"
 }
 
 variable "aws_access_key" {
