a77620f9ceba002bbdd56c532295aedbfbf35b2d
eladioltb <eladioloriente@gmail.com>
2025-08-01 10:28:49 +0200

    fix: Added cloudfront_distribution_arn to output module

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -1,3 +1,7 @@
 output "cf_distribution_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "cfº_distribution_arn" {
+  value = aws_cloudfront_distribution.s3_distribution.arn
+}
