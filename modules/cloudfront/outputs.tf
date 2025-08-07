ca36af25bdf320b53cc0fb4a77b6c70018938eb3
Eladio Loriente <eladioloriente@gmail.com>
2025-08-07 17:30:01 +0200

    feat: Added OAC import and changed the cf outputs

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -1,7 +1,11 @@
+locals {
+  cf_arn = aws_cloudfront_distribution.s3_distribution.arn
+}
+
 output "cf_distribution_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
 output "cloudfront_distribution_arn" {
-  value = aws_cloudfront_distribution.s3_distribution.arn
+  value = local.cf_arn
 }
